8952d63b2b831e7f5459b608abcc1bd8db669c2a
Charlie <charliemartin190904@gmail.com>
2025-12-24 21:34:22 +0700

    Commit No. 1 – Init template

--- app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
+++ app/src/main/java/com/example/test_lab_week_12/api/MovieService.kt
@@ -1,4 +1,13 @@
 package com.example.test_lab_week_12.api
 
+import com.example.test_lab_week_12.model.PopularMoviesResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String
+    ): PopularMoviesResponse
 }
