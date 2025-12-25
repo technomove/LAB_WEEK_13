a1ac43fe94a5bcec19e91d1505d015ac9f44b8c7
Charlie <charliemartin190904@gmail.com>
2025-12-25 16:09:15 +0700

    Commit No. 2 – Building app with coroutines

--- app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieRepository.kt
@@ -1,7 +1,28 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.test_lab_week_12.api.MovieService
+import com.example.test_lab_week_12.model.Movie
 
 class MovieRepository(private val movieService: MovieService) {
 
+    private val apiKey = "ISI_API_KEY_KAMU"
+
+    private val movieLiveData = MutableLiveData<List<Movie>>()
+    val movies: LiveData<List<Movie>>
+        get() = movieLiveData
+
+    private val errorLiveData = MutableLiveData<String>()
+    val error: LiveData<String>
+        get() = errorLiveData
+
+    suspend fun fetchMovies() {
+        try {
+            val response = movieService.getPopularMovies(apiKey)
+            movieLiveData.postValue(response.results)
+        } catch (e: Exception) {
+            errorLiveData.postValue("An error occurred: ${e.message}")
+        }
+    }
 }
--- app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
+++ app/src/main/java/com/example/test_lab_week_12/MovieViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.test_lab_week_12
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class MovieViewModel(private val movieRepository: MovieRepository) : ViewModel() {
+class MovieViewModel(
+    private val movieRepository: MovieRepository
+) : ViewModel() {
 
+    init {
+        fetchPopularMovies()
+    }
+
+    val popularMovies: LiveData<List<com.example.test_lab_week_12.model.Movie>>
+        get() = movieRepository.movies
+
+    val error: LiveData<String>
+        get() = movieRepository.error
+
+    private fun fetchPopularMovies() {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieRepository.fetchMovies()
+        }
+    }
 }
